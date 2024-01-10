c9e71768386deef401f6616caff04e5ff7b50c7c
Cosmo12978 <153873427+Cosmo12978@users.noreply.github.com>
2024-01-09 23:48:23 -0500

    Add files via upload

---
+++ test/derma/cl_craftingtab.lua
@@ -0,0 +1,7 @@
+hook.Add("CreateMenuButtons", "Craftingmenu", function(tabs)
+    
+        tabs["Crafting"] = function(container) 
+            container:Add("") -- container name
+        
+    end
+end)
---
+++ test/sh_plugin.lua
@@ -0,0 +1,8 @@
+local PLUGIN = PLUGIN or {}
+
+PLUGIN.name = "Make crafting tab pop up hopefully"
+PLUGIN.author = "Cosmos"
+PLUGIN.description = "Hope this helps!"
+
+
+
